31e04355ceb69f1d497f1bb087752536f313bce8
Phil Hord <phord@purestorage.com>
2020-01-09 12:08:25 -0800

    Use a string for chars instead of a list
    Purely stylistic. No functional change.

--- genetic1.1.py
+++ genetic1.1.py
@@ -1,14 +1,7 @@
 import random
 import matplotlib.pyplot as plt
 
-characters = [' ', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's',
-              'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n',
-              'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S',
-              'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N',
-              'M', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_',
-              '=', '+', '[', ']', '{', '}', ';', "'", ',', '.', '/', ':', '"',
-              '<', '>', '?', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-              "'"]
+characters = ' qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()-_=+[]{};'',./:"<>?1234567890'
 
 
 class genesis:
